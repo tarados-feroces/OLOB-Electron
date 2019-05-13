ebd25cbc7957e356ab71bff0c8d837ebdc1a261e
Danchetto <danchetto@yandex.ru>
2019-05-14 00:41:56 +0300

    new popups

--- src/components/ConfirmPopup/index.tsx
+++ src/components/ConfirmPopup/index.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { block } from 'bem-cn';
 
 import './index.scss';
-import { Modal } from 'semantic-ui-react';
 import Button from '../../ui/Button';
 
 interface ConfirmPopupProps {
@@ -18,14 +17,14 @@ const b = block('olob-confirm-popup');
 export const ConfirmPopup: React.FunctionComponent<ConfirmPopupProps> =
     ({ text, confirmButtonText, declineButtonText, onAccept, onDecline }) => {
         return (
-            <Modal>
-                <Modal.Header>{text}</Modal.Header>
-                <Modal.Content>
+            <div className={b()}>
+                <div className={b('text')}>
+                    <p className={b('info')}>{text}</p>
+                </div>
                     <div className={b('button-row')}>
                         <Button className={b('button').toString()} onClick={onAccept}>{confirmButtonText || 'Ок'}</Button>
                         <Button className={b('button').toString()} onClick={onDecline}>{declineButtonText || 'Неа'}</Button>
                     </div>
-                </Modal.Content>
-            </Modal>
+            </div>
         );
     };
